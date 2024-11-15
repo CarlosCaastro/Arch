5bc2a4319a88737db8a4ddacc858c4ffd9f2ab27
Carlos Castro <carlos.candradr@gmail.com>
2024-11-15 17:24:27 -0300

    Adição do Hive-Metastore com Postgres

--- jobs/teste.py
+++ jobs/teste.py
@@ -1,17 +1,35 @@
-import pyspark
+from pyspark.sql import SparkSession
 from delta import *
 
-builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+# Configuração do Spark para Delta e MinIO
+spark = SparkSession.builder \
+    .appName("DeltaWithMinIO") \
     .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
     .config("spark.hadoop.fs.s3a.access.key", "minio") \
     .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.sql.warehouse.dir", "/opt/bitnami/spark/warehouse") \
+    .config("hive.metastore.uris", "thrift://hive-metastore:9083") \
+    .enableHiveSupport() \
+    .getOrCreate()
+    #
+# Caminho no MinIO para os dados Delta
+delta_table_path = "s3a://spark/delta/tabela_teste"
 
-spark = configure_spark_with_delta_pip(builder).getOrCreate()
+# Escreve dados no formato Delta no MinIO
+# data = spark.range(0, 5)
+# data.write.format("delta").mode("overwrite").save(delta_table_path)
 
-data = spark.range(0, 5)
+# Criação da tabela EXTERNAL no Hive Metastore
+spark.sql(f"""
+CREATE EXTERNAL TABLE IF NOT EXISTS default.tabela (
+    id BIGINT
+)
+USING delta
+LOCATION '{delta_table_path}'
+""")
 
-data.write.format("delta").save("s3a://spark/tabela_teste")
+print("Tabela registrada no Hive Metastore com sucesso!")
