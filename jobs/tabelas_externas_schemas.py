82578ace2db6101a7ef3ae0a0e827cab769d9917
Carlos Castro <carlos.candradr@gmail.com>
2024-11-17 15:40:37 -0300

    Schemas e tabelas, pipeline

--- jobs/tabelas_externas_schemas.py
+++ jobs/tabelas_externas_schemas.py
@@ -1,33 +1,3 @@
-# from pyspark.sql import SparkSession
-
-# # Inicializa a sessão Spark com suporte ao Delta Lake
-# spark = SparkSession.builder \
-#     .appName("Create External Table for Delta Format") \
-#     .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
-#     .config("spark.hadoop.fs.s3a.access.key", "minio") \
-#     .config("spark.hadoop.fs.s3a.secret.key", "minio123") \
-#     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-#     .config("spark.sql.catalogImplementation", "hive") \
-#     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-#     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-#     .config("javax.jdo.option.ConnectionURL", "jdbc:postgresql://postgres:5432/airflow") \
-#     .config("javax.jdo.option.ConnectionUserName", "airflow") \
-#     .config("javax.jdo.option.ConnectionPassword", "airflow") \
-#     .enableHiveSupport() \
-#     .getOrCreate()
-
-# # Cria a tabela externa Delta no Hive
-# create_table_query = """
-# CREATE EXTERNAL TABLE d_calendar
-# USING delta
-# LOCATION 's3a://ifood/gold/d_calendar/files'
-# """
-
-# # Executa o comando SQL para criar a tabela
-# spark.sql(create_table_query)
-
-# print("Tabela externa 'd_calendar' criada com sucesso para dados em formato Delta.")
-
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
@@ -49,6 +19,22 @@ schemas = ["bronze", "silver", "gold"]
 for schema in schemas:
     spark.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
     print(f"Schema '{schema}' criado com sucesso.")
+
+
+drop_queries = {
+    "bronze.yellow_taxi": """
+        DROP TABLE IF EXISTS bronze.yellow_taxi;
+    """,
+    "silver.yellow_taxi": """
+        DROP TABLE IF EXISTS silver.yellow_taxi;
+    """,
+    "gold.f_yellow_taxi": """
+        DROP TABLE IF EXISTS gold.f_yellow_taxi;
+    """,
+    "gold.d_calendar": """
+        DROP TABLE IF EXISTS gold.d_calendar;
+    """
+}
 
 tables_queries = {
     "bronze.yellow_taxi": """
@@ -72,6 +58,10 @@ tables_queries = {
         LOCATION 's3a://ifood/gold/d_calendar/d_calendar'
     """
 }
+
+for table, query in drop_queries.items():
+    spark.sql(query)
+    print(f"Tabela externa '{table}' dropada.")
 
 for table, query in tables_queries.items():
     spark.sql(query)
